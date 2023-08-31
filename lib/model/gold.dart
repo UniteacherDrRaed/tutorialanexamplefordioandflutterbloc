83664219303cf54de01693f803c90e97b0cd6bc0
Dr. Raed <drraed.jb@gmail.com>
2023-09-01 00:33:01 +0200

    adding GOLD model

--- lib/model/gold.dart
+++ lib/model/gold.dart
@@ -1,14 +1,14 @@
 class Gold{
-  final double price_gram_24k;
-  final double price_gram_22k;
-  final double price_gram_21k;
-  final double price_gram_20k;
-  final double price_gram_18k;
+  final double priceGram24k;
+  final double priceGram22k;
+  final double priceGram21k;
+  final double priceGram20k;
+  final double priceGram18k;
   Gold({
-    required this.price_gram_24k,
-    required this.price_gram_22k,
-    required this.price_gram_21k,
-    required this.price_gram_20k,
-    required this.price_gram_18k
+    required this.priceGram24k,
+    required this.priceGram22k,
+    required this.priceGram21k,
+    required this.priceGram20k,
+    required this.priceGram18k
   });
 }
